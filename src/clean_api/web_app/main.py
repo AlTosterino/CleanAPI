6af90d9daa8fc53f0f189270d872d34ffbaff364
AlTosterino <altosterino@gmail.com>
2022-03-31 21:29:22 +0200

    :package: Add `TortoiseORM`

--- src/clean_api/web_app/main.py
+++ src/clean_api/web_app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
-app = FastAPI()
+app = FastAPI(title="Clean API")
 
 
 @app.get("/")
@@ -10,3 +11,12 @@ async def root() -> dict[str, str]:
 
 def placeholder() -> bool:
     return True
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://:memory:",
+    modules={"models": []},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
